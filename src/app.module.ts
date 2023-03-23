d6d00c54d6b3c33ea25294a4a69fb38515a53aa4
Anupam Kumar <anupamurl@gmail.com>
2023-03-23 23:43:39 +0530

    Swagger Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { BlogsModule } from './blogs/blogs.module';
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/(.*)'],
+    }),
+    UsersModule,
+    BlogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { UsersModule } from './users/users.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+  .setTitle('Swagger UI')
+  .setDescription('Nest js swagger')
+  .setVersion('1.0') 
+  .build();
+  
+ 
+
+const document = SwaggerModule.createDocument(app, config, {
+  include: [UsersModule]   
+});
+
+
+SwaggerModule.setup('api', app, document);
+
+
+
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
